4e087300070ac3e28389b84fc676097275be4ca0
Jakob Kielland <jakob-ek@hotmail.com>
2024-05-30 09:56:14 +0200

    update name list

--- crimes-against-algdat/bogopick.py
+++ crimes-against-algdat/bogopick.py
@@ -1,14 +1,16 @@
 from random import shuffle
 
-# Names are duplicated since O(6!) is no match for my microwave, let alone my M1 Mac. O(11!) is more fun.
-names = ["Hilde_1", "Ivar_1", "Karl Yngve_1", "Robin_1", "Sunniva_1", "Hilde_2", "Ivar_2", "Karl Yngve_2", "Robin_2", "Sunniva_2"]
+names = ["Hilde", "Ingvild", "Vegard", "Kjartan",
+         "Sondre", "Sunniva", "Karl Yngve", "Ivar", "Robin"]
 sorted_names = sorted(names)
 
-iterations = 0 # in reality this is O(N) less than the actual complexity, since it doesn't take shuffling into account
+# in reality this is O(N) less than the actual complexity, since it doesn't take shuffling into account
+iterations = 0
 
 while (names != sorted_names):
-  candidate = names[0]
-  shuffle(names)
-  iterations += 1
+    candidate = names[0]
+    shuffle(names)
+    iterations += 1
 
-print("The lucky winner after only", iterations, "iterations is:", candidate[0:-2])
+print("The lucky winner after only", iterations,
+      "iterations is:", candidate)
